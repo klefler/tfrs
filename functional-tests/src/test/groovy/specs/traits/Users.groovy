5b3a629fb71778dddc258378068906e0b4ea4351
Nick Phura <Nick.Phura@quartech.com>
2018-09-26 10:29:18 -0700

    Add prints to Users.groovy

--- functional-tests/src/test/groovy/specs/traits/Users.groovy
+++ functional-tests/src/test/groovy/specs/traits/Users.groovy
@@ -6,18 +6,25 @@ package traits
 trait Users {
   Map env = System.getenv()
   Map getSendingFuelSupplier() {
-    [username:env['SUPPLIER_ONE_USERNAME'], password:env['SUPPLIER_ONE_PASSWORD']]
+    logUser([username:env['SUPPLIER_ONE_USERNAME'], password:env['SUPPLIER_ONE_PASSWORD']])
   }
 
   Map getReceivingFuelSupplier() {
-    [username:env['SUPPLIER_TWO_USERNAME'], password:env['SUPPLIER_TWO_PASSWORD'], org:env['SUPPLIER_TWO_ORG']]
+    logUser([username:env['SUPPLIER_TWO_USERNAME'], password:env['SUPPLIER_TWO_PASSWORD'], org:env['SUPPLIER_TWO_ORG']])
   }
 
   Map getAnalyst() {
-    [username:env['ANALYST_USERNAME'], password:env['ANALYST_PASSWORD']]
+    logUser([username:env['ANALYST_USERNAME'], password:env['ANALYST_PASSWORD']])
   }
 
   Map getDirector() {
-    [username:env['DIRECTOR_USERNAME'], password:env['DIRECTOR_PASSWORD']]
+    logUser([username:env['DIRECTOR_USERNAME'], password:env['DIRECTOR_PASSWORD']])
+  }
+
+  Map logUser(Map user) {
+    println("======================================")
+    println(user)
+    println("======================================")
+    return user
   }
 }
